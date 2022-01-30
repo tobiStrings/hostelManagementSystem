9a3c709be5853fd6f40cefbba4a1c4cc598f941a
TobiStrings <ligalititobiloluwa2005@gmail.com>
2022-01-29 23:54:11 -0800

    models

---
+++ models/student_model.go
@@ -0,0 +1,20 @@
+package models
+
+import (
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"time"
+)
+type Student struct {
+	Id  primitive.ObjectID `bson:"id"`
+	FirstName string `json:"first_name" binding:"required,min=2,max=30"`
+	LastName string `json:"last_name" binding:"required,min=2,max=30"`
+	Age          uint64    `json:"age" binding:"required"`
+	MatricNumber string    `json:"matric_number" binding:"required,min=6,max=6"`
+	DateCreated  time.Time `json:"date_created"`
+	DateUpdated time.Time `json:"date_updated"`
+	StudentId string `json:"student_id"`
+}
+
+func NewStudent(firstName string, lastName string, age uint64, maricNumber string) *Student {
+	return &Student{FirstName: firstName, LastName: lastName, Age: age, MatricNumber: maricNumber}
+}
