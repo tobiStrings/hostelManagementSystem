e4646090da7af40bf672b01ebcaed2732163e52a
TobiStrings <ligalititobiloluwa2005@gmail.com>
2022-02-01 16:57:48 -0800

    student_service

--- service/student_service.go
+++ service/student_service.go
@@ -3,7 +3,6 @@ package service
 import (
 	"context"
 	"errors"
-	"fmt"
 	"github.com/go-playground/validator/v10"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
@@ -36,50 +35,43 @@ var validate = validator.New()
 var studentCollection *mongo.Collection = repositories.OpenCollection(repositories.Client,"student")
 
 
-func (studentService *StudentServiceImp) SaveStudent(dto dtos.StudentDto) (models.Student, error){
-	var ctx,cancel =context.WithTimeout(context.Background(),100*time.Second)
+func (studentService *StudentServiceImp) SaveStudent(dto dtos.StudentDto) (models.Student, error) {
+	var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
 
-	foundStudent := models.Student{}
-
-	matric_number :=dto.MaricNumber
-
-	defer cancel()
-		st := studentCollection.FindOne(ctx, bson.M{"matric_number": matric_number}).Decode(&foundStudent)
-		if st != nil{
-			fmt.Println("Error")
-			fmt.Println(st.Error())
-			return models.Student{}, errors.New(st.Error())
-		}
-		if &foundStudent != nil{
-			return foundStudent,errors.New("user already exist")
-		}
-
-
-	var newStudent models.Student
-
-	newStudent = MapDtoToStudent(dto,newStudent)
-
-
-	validationErr := validate.Struct(newStudent)
-
-	if validationErr != nil{
-		return models.Student{},errors.New("user not created because validation cannot be done")
-	}
-
-	newStudent.DateCreated,_ = time.Parse(time.RFC3339,time.Now().Format(time.RFC3339))
-	newStudent.DateUpdated,_ = time.Parse(time.RFC3339,time.Now().Format(time.RFC3339))
-	newStudent.Id =primitive.NewObjectID()
-	newStudent.StudentId = newStudent.Id.Hex()
-
-	_,insertError := studentCollection.InsertOne(ctx,newStudent)
-
-	if insertError != nil {
-		return models.Student{},errors.New("user not created because student cannot be inserted")
-	}
+	var result models.Student
 
 	defer cancel()
 
-	return newStudent,nil
+	err2 := studentCollection.FindOne(ctx,bson.M{"matricnumber":dto.MaricNumber}).Decode(&result)
+
+	if err2 != nil{
+		var newStudent models.Student
+
+		newStudent = MapDtoToStudent(dto, newStudent)
+
+		validationErr := validate.Struct(newStudent)
+
+		if validationErr != nil {
+			return models.Student{}, errors.New("user not created because validation cannot be done")
+		}
+
+		newStudent.DateCreated, _ = time.Parse(time.RFC3339, time.Now().Format(time.RFC3339))
+		newStudent.DateUpdated, _ = time.Parse(time.RFC3339, time.Now().Format(time.RFC3339))
+		newStudent.Id = primitive.NewObjectID()
+		newStudent.StudentId = newStudent.Id.Hex()
+
+		_, insertError := studentCollection.InsertOne(ctx, newStudent)
+
+		if insertError != nil {
+			return models.Student{}, errors.New("user not created because student cannot be inserted")
+		}
+
+		defer cancel()
+
+		return newStudent, nil
+	}
+
+	return result,errors.New("student already exist")
 }
 
 func (studentService *StudentServiceImp) GetStudentById(studentId string) (models.Student,error) {
