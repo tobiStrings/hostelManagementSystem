8e887511ce6285041ed112474032cde9f153ffed
TobiStrings <ligalititobiloluwa2005@gmail.com>
2022-02-08 10:17:50 -0800

    SaveRoomdto

--- dtos/SaveRoomDto.go
+++ dtos/SaveRoomDto.go
@@ -1,7 +1,7 @@
 package dtos
 
 type SaveRoomDto struct {
-	RoomName string
-	RoomNumber uint64
-	NumberOfAvailableBeds uint64
+	RoomName string `json:"room_name" binding:"required,min=4,max=12"`
+	RoomNumber uint64 `json:"room_number" binding:"required,min=1,max=8"`
+	NumberOfAvailableBeds uint64  `json:"number_of_available_beds" binding:"required,min=0,max=15"`
 }
