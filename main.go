aa15cd16ff566dbfcb6e037e7ebe93ac5439c07e
TobiStrings <ligalititobiloluwa2005@gmail.com>
2022-02-08 10:18:18 -0800

    Main

--- main.go
+++ main.go
@@ -7,16 +7,17 @@ import (
 )
 
 func main() {
-	os.Setenv("PORT","9090")
+	os.Setenv("PORT","8080")
 	port:= os.Getenv("PORT")
 
 	if port == ""{
-		port = "9090"
+		port = "8080"
 	}
 
 	router := gin.New()
 	router.Use(gin.Logger())
 	routes.StudentRoute(router)
+	routes.RoomRoutes(router)
 
 	router.GET("/api-1", func(c *gin.Context) {
 		c.JSON(200,gin.H{"success":"Access granted for api-1"})
