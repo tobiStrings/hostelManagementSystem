ebafb469e98ae8b035bec64c1b84c6db7144b9dc
TobiStrings <ligalititobiloluwa2005@gmail.com>
2022-02-08 10:18:50 -0800

    room_routes

--- routes/room_routes.go
+++ routes/room_routes.go
@@ -1,1 +1,10 @@
 package routes
+
+import (
+	"github.com/gin-gonic/gin"
+	"hostelManagementSystem/controller"
+)
+
+func RoomRoutes(incomingRoutes *gin.Engine)  {
+	incomingRoutes.POST("/room",controller.CreateRoom())
+}
